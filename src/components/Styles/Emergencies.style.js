e92271caeb4f0f89650dd545314c3665e2e1c016
3vilBonobo <iboutsika@gmail.com>
2022-11-22 11:58:02 +0200

    add Emergencies styles

---
+++ src/components/Styles/Emergencies.style.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const StyledEmergenciesMainSection = styled.div`
+  background-color: #fff;
+  background-image: url('images/tooth_bg.svg');
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  padding: 3rem 1rem;
+  position: relative;
+  top: 180px;
+`;
+
+export const StyledEmergenciesTitle = styled.h1`
+  align-self: start;
+  padding-left: 6rem;
+`;
+
+export const StyledEmergenciesContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 3rem 6rem;
+`;
+
+export const StyledEmergenciesInfo = styled.div`
+  text-align: justify;
+  padding: 1rem 6rem 0 0;
+  max-width: 60%;
+`;
+
+export const StyledEmergenciesImage = styled.img`
+  max-width: 400px;
+  height: auto;
+  border-radius: 50%;
+`;
--- src/components/routes/Emergencies.js
+++ src/components/routes/Emergencies.js
@@ -1,11 +1,27 @@
 import { patients } from '../../data/data';
+import {
+  StyledEmergenciesMainSection,
+  StyledEmergenciesTitle,
+  StyledEmergenciesContainer,
+  StyledEmergenciesInfo,
+  StyledEmergenciesImage,
+} from '../styles/Emergencies.style';
 
 const Emergencies = () => {
   return (
-    <div>
-      <h1>{patients.emergencies.title}</h1>
-      <p>{patients.emergencies.info}</p>
-    </div>
+    <StyledEmergenciesMainSection>
+      <StyledEmergenciesTitle>
+        {patients.emergencies.title}
+      </StyledEmergenciesTitle>
+      <StyledEmergenciesContainer>
+        <StyledEmergenciesInfo
+          dangerouslySetInnerHTML={{
+            __html: patients.emergencies.info,
+          }}
+        ></StyledEmergenciesInfo>
+        <StyledEmergenciesImage src={patients.emergencies.image} />
+      </StyledEmergenciesContainer>
+    </StyledEmergenciesMainSection>
   );
 };
 
