15ea36bff0a375ae8255920caabf6b00d92ed738
3vilBonobo <iboutsika@gmail.com>
2022-11-29 17:06:17 +0200

    fix mobile treatment styles

--- src/components/Styles/Treatments.style.js
+++ src/components/Styles/Treatments.style.js
@@ -3,18 +3,17 @@ import { Link } from 'react-router-dom';
 
 export const StyledTreatmentsMainSection = styled.div`
   background-color: ${(props) => props.theme.white};
-  background-image: url('images/tooth_bg.svg');
-  background-size: cover;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: flex-start;
+  justify-content: center;
   width: 100%;
   position: relative;
   top: 180px;
   padding: 4rem 1rem 5rem 1rem;
   @media screen and (min-width: 768px) {
-    padding: 3rem 5rem;
+    justify-content: flex-start;
+    padding: 6rem 5rem;
     top: 140px;
   }
 `;
@@ -27,13 +26,20 @@ export const StyledTreatmentsTopContainer = styled.div`
 export const StyledTreatmentsBottomContainer = styled.div`
   width: 100%;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: flex-start;
   justify-content: center;
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  }
 `;
 
 export const StyledTreatmentsMainTitle = styled.h1`
-  margin: 0 5rem 2rem 10rem;
+  padding-bottom: 2rem;
+  @media screen and (min-width: 768px) {
+    padding-bottom: 0;
+    margin: 0 5rem 2rem 10rem;
+  }
 `;
 
 export const StyledTreatmentsMainInfo = styled.div`
@@ -64,18 +70,25 @@ export const StyledTreatmentsLinkContainer = styled.div`
   flex-direction: column;
   align-items: flex-start;
   justify-content: center;
-  margin-top: 8rem;
+  margin-top: 1rem;
 `;
 export const StyledTreatmentsMainLink = styled(Link)`
   text-decoration: none;
   font-size: 1.1rem;
   font-weight: 500;
-  padding: 1rem 1.5rem;
+  padding: 0.6rem 1rem;
+
   color: ${(props) => props.theme.darkBlue};
   border: 4px solid ${(props) => props.theme.darkBlue};
   border-radius: 10px;
   min-width: 330px;
-  margin-bottom: 5px;
+  margin-bottom: 2px;
+  @media screen and (min-width: 768px) {
+    text-align: left;
+    padding: 1rem 1.5rem;
+    min-width: 330px;
+    margin-bottom: 5px;
+  }
 `;
 export const StyledTreatmentsOutletContainer = styled.div`
   display: flex;
@@ -83,20 +96,28 @@ export const StyledTreatmentsOutletContainer = styled.div`
   align-items: center;
   justify-content: space-around;
   width: 100%;
+  padding: 4rem;
   @media screen and (min-width: 768px) {
-    padding: 3rem;
+    padding: 0 3rem;
     align-items: flex-start;
-    min-height: 500px;
+    min-height: 400px;
   }
 `;
 export const StyledTreatmentOutletSection = styled.div`
+  background-color: ${(props) => props.theme.white};
+  background-image: url('images/tooth_bg.svg');
+  background-size: cover;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   padding: 1rem;
   position: relative;
-  height: 500px;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    height: 400px;
+  }
 `;
 
 export const StyledTreatmentOutletLeftContainer = styled.div`
@@ -108,6 +129,10 @@ export const StyledTreatmentOutletLeftContainer = styled.div`
 
 export const StyledTreatmentOutletImage = styled.img`
   border-radius: 50%;
-  max-width: 400px;
-  height: auto;
+  padding: 1rem;
+  max-width: 300px;
+  @media screen and (min-width: 768px) {
+    max-width: 400px;
+    height: auto;
+  }
 `;
