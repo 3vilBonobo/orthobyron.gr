a29b9a4b1bb4741bb74288c6727305d81050bb5e
3vilBonobo <iboutsika@gmail.com>
2023-03-10 14:33:14 +0200

    fix navbar click outside functionality

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
-// import { useEffect } from 'react';
-// import { useLocation } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
 import {
   StyledBurgerIcon,
   StyledCloseIcon,
@@ -8,17 +8,34 @@ import {
 } from '../styles/Navbar.style';
 import { menuItemsData } from '../../data/data';
 import MenuItem from './MenuItem';
-import { useState } from 'react';
 
 const Navbar = () => {
   const [sidebar, setSidebar] = useState(false);
   const ShowSidebar = () => {
     setSidebar(!sidebar);
   };
-  // const { pathname } = useLocation();
-  // useEffect(() => {
-  //   setSidebar(false);
-  // }, [pathname]);
+  const { pathname } = useLocation();
+  useEffect(() => {
+    setSidebar(false);
+  }, [pathname]);
+
+  const menuRef = useRef();
+
+  const handleOutsideClicks = (event) => {
+    if (
+      !sidebar &&
+      menuRef.current &&
+      !menuRef.current.contains(event.target)
+    ) {
+      setSidebar(false);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideClicks);
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClicks);
+    };
+  });
 
   return (
     <>
@@ -28,7 +45,12 @@ const Navbar = () => {
         <StyledMenuItems>
           {menuItemsData.map((items, index) => {
             return (
-              <MenuItem ShowSidebar={ShowSidebar} key={index} items={items} />
+              <MenuItem
+                ref={menuRef}
+                ShowSidebar={ShowSidebar}
+                key={index}
+                items={items}
+              />
             );
           })}
         </StyledMenuItems>
